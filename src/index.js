ebbd213870fb60284416ec867debde027ee8ffb9
jheave <jimenezjheave123@gmail.com>
2022-06-29 22:34:46 +0800

    add find winner

--- src/index.js
+++ src/index.js
@@ -76,6 +76,29 @@ class Game extends React.Component {
     }
 }
 
+function calculateWinner(Square) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    lines.reduce((acc, line) => {
+        if (acc === null) {
+            return acc;
+        }
+        if (Square[line[0]] === Square[line[1]] && Square[line[1]] === Square[line[2]]) {
+            return Square[line[0]];
+        }
+        return acc;
+    });
+
+}
+
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
