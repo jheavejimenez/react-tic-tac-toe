c4953053fdffa7697711cbc6e8ecb6145ebcb8cf
jheave <jimenezjheave123@gmail.com>
2022-06-29 22:21:07 +0800

    added xIsnext ternary operations

--- src/index.js
+++ src/index.js
@@ -22,7 +22,10 @@ class Board extends React.Component {
     handleClick(i) {
         const squares = this.state.squares.slice();
         squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({squares: squares});
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
     renderSquare(i) {
         return <Square
